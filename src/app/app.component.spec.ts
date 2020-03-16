5ea477133229b3138114aac6cb89edf7e74bc856
luo0818 <57941664+luo0818@users.noreply.github.com>
2020-03-16 22:58:00 +0800

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'progress02'`, () => {
+  it(`should have as title 'angulardemo09'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('progress02');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('angulardemo09');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('progress02 app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angulardemo09!');
   });
 });
